bb92f68d7fe70f21afdcb965344c2ab4af923130
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2024-11-28 21:23:26 +0500

    Initial Commit

---
+++ MailTabBar/MailTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailTabBarApp.swift
+//  MailTabBar
+//
+//  Created by Benji Loya on 28.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MailTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
